743d82e8160379b011ca56b6290059a2f409bbf2
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-10-03 00:48:23 +0100

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,3 +1,7 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
 
@@ -10,3 +14,4 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
+#endif /* LISTS_H */

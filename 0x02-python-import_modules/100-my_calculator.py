05a466f22bfe135d3f316766a2eea2d22997e6bd
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-10-06 20:05:03 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
  from  calculator_1 import add sub mul div
  from sys import argv
  if len(argv) != 4:
-    print(Usage: ./100-my_calculator.py <a> <operator> <b>)
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     return 1
  else :
     if argv[2] == "+":

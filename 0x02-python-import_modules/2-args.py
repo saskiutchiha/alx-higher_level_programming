409341c6f989774ba347d494e1c39e59cef6a289
saskiutchiha <boulidamabdellah8@gmail.com>
2023-10-06 18:03:43 +0100

    kk

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
-a = sys.argv
-print("{} argument".format(len(a)-1),end="")
-i = len(a)-1
-if i == 0 :
- print("s.")
-else :
+if __name__ == "__main__" :
+    
+ import sys
+ a = sys.argv
+ print("{} argument".format(len(a)-1),end="")
+ i = len(a)-1
+ if i == 0 :
+  print("s.")
+ else :
     if (i ==1):
      print(":")
     else :
         print("s:")
-while(i>0):
- print("{}: {}".format(len(a)-i,a[len(a)-i]))
- i-=1
-
+ while(i>0):
+  print("{}: {}".format(len(a)-i,a[len(a)-i]))
+  i-=1

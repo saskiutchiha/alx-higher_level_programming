9d88ebb096674a38bbb7db5c68f27d149ef3a134
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-10-08 13:21:15 +0100

    Create 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin.python3
+def print_matrix_integer(matrix=[[]]):
+ for i in range(len(matrix)):
+   for j in range(len(matrix[0])-1):
+    print("{:d}".format(matrix[i][j]),end=" ")
+   print("{:d}".format(matrix[i][j+1]),end="") 
+   print("$")

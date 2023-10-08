100c8303af27a794b44d2870ba54720b01c125a9
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-10-08 13:32:02 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
  for i in range(len(matrix)):
    for j in range(len(matrix[0])-1):
-    print(i,end=" ")
-   print(i,end="") 
+    print(matrix[i][j],end=" ")
+   print(matrix[i][j+!],end="") 
    print("$")

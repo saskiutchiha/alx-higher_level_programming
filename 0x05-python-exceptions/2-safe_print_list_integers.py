3c36ea5a3070dfaf0d4155eca60b1af841534120
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-10-23 22:31:16 +0100

    Create 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+def safe_print_list_integers(my_list=[], x=0):
+         a = 0
+         while a < x:
+          try:
+           print("{:d}".format(my_list[a]))
+           a+=1
+          except TypeError:
+             my_list = my_list[:2] + my_list[a+1:]
+             continue  
+ 

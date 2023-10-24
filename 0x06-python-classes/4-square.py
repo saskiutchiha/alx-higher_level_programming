cc05bf5c230049f793e848dfc3c8beb7e1e413d0
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-10-24 19:35:14 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,7 +3,7 @@
 
 class Square:
     """Represent a square."""
-    def __init__(self, size):
+    def __init__(self, size=0):
        """Initialize a new Square.
        Args:
        size (int): The size of the new square.

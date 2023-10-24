a6a7f12b6b97c318d146ed310eec5e499a4facde
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-10-24 20:34:57 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,7 +14,7 @@ class Square:
         if not  isinstance(size, int):
          raise TypeError("size must be an integer")
         elif size < 0:
-        raise ValueError("size must be >= 0")
+         raise ValueError("size must be >= 0")
         else :
          self.__size = size 
         if (not isinstance(value, tuple)) or len(value) != 2 or (value[0] < 0 or value[1] < 0):

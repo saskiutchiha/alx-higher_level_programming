27b45ff654333752f22c31ad69f83dd9b5e3983a
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-10-24 20:13:46 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,6 +28,7 @@ class Square:
         self.__size = value
     @property  
     def position(self):
+       """Get/set the current position of the square."""  
       return self.__position
     @position.setter  
     def position(self,value):

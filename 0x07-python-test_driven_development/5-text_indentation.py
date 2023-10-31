32e6cd9d17a48bad4f8efae22c083e301a07968d
saskiutchiha <boulidamabdellah8@gmail.com>
2023-10-31 21:32:29 +0100

    ll

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def text_indentation(text):
     if not isinstance(text,str):
         raise TypeError("text must be a string")
--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def max_integer(list=[]):
         """Function to find and return the max integer in a list of integers
         If the list is empty, the function returns None

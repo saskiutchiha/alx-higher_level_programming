942179d214e90f9d645150dbedff9d423307cd84
saskiutchiha <boulidamabdellah8@gmail.com>
2023-10-31 15:01:27 +0100

    ll

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+def print_square(size):
+    if not isinstance(size,int):
+        raise TypeError("size must be an integer")
+    elif size < 0 :
+        raise ValueError("size must be >= 0")
+    else :
+        for i in range(size):
+            for j in range(size):
+                print("#",end="")
+            print("")
+            

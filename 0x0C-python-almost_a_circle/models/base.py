ec1e99dded8ec1f83d0f88f21d5aabc852a64c13
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-11-13 21:20:37 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+""" define the classe """
+
 class Base :
     __nb_objects = 0
     def __init__(self,id=None):
+        """ initialse id of object"""
         if (id != None):
             self.id = id
         else:

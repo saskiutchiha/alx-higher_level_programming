4acf1ea5d9020edd7335486d177ea440a6704105
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-01-17 21:01:00 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
  a = sys.argv()
  db = my.connect(host="localhost", port=3306, user=a[0], passwd=a[1], db=a[2])
  cursor = db.cursor()
- cursor.execute("select * from states order by states.id")
+ cursor.execute("select * from states order by states.id;")
  for data in cursor :
     print(data)
  cursor.close()

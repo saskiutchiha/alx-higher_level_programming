1266a8ec357a34a47d1962ca72ecea6eba18aa6e
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-01-17 20:55:20 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,43 +1,16 @@
 #!/usr/bin/python3
-"""
-This script lists all states from a MySQL database.
+"lists all states"
 
-The script connects to a MySQL database using the provided command line arguments,
-executes a SQL query to fetch all states ordered by their ID, and prints the result.
-
-Command line arguments:
-    1. MySQL username
-    2. MySQL password
-    3. MySQL database name
-
-Dependencies:
-    - MySQLdb: A Python DB API-2.0-compliant interface to MySQL.
-    - sys: Provides access to some variables used or maintained by the Python interpreter.
-
-Usage:
-    python3 script.py <username> <password> <database>
-"""
 
 import MySQLdb as my
 import sys 
-
-# Parse command line arguments
-a = sys.argv()
-
-# Connect to the MySQL database
-db = my.connect(host="localhost", port=3306, user=a[0], passwd=a[1], db=a[2])
-
-# Create a cursor object
-cursor = db.cursor()
-
-# Execute SQL query
-cursor.execute("select * from states order by states.id")
-
-# Fetch and print all rows from the query
-for data in cursor :
-        print(data)
-
-# Close the cursor and the database connection
-cursor.close()
-db.close()
+if __main__ == "__name__":
+ a = sys.argv()
+ db = my.connect(host="localhost", port=3306, user=a[0], passwd=a[1], db=a[2])
+ cursor = db.cursor()
+ cursor.execute("select * from states order by states.id")
+ for data in cursor :
+    print(data)
+ cursor.close()
+ db.close()
 

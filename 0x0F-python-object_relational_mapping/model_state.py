b798f9c002bfeeb7753fe276902db2481e32863f
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-01-20 00:18:43 +0100

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,4 +7,4 @@ from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
         engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}' .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-         Base.metadata.create_all(engine)
+        Base.metadata.create_all(engine)

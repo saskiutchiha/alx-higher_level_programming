2d65a68fddec13ffaf58c4e5b3050e8bf07c03b1
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-01-19 23:19:37 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
      db = my.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
      cursor = db.cursor()
-     cursor.execute("select * from states where name like 'N%'order by states.id;")
+     cursor.execute("select * from states where name like 'N%' order by states.id;")
      for data in cursor :
                 print(data)
      cursor.close()
